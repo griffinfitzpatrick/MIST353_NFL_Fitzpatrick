a71f03c96971dcfce1452f080e1b340204f02a6b
Griffin Fitzpatrick <gef00007@mix.wvu.edu>
2026-03-11 23:34:08 -0400

    push

--- API/get_db_connection.py
+++ API/get_db_connection.py
@@ -1,6 +1,6 @@
 import os 
 import pyodbc
-from dotnenv import load_dotenv
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -9,6 +9,6 @@ def get_db_connection():
     database = os.getenv('DB_NAME')
     username = os.getenv('DB_LOGIN')
     password = os.getenv('DB_PASSWORD')
-
+    #OBDC Driver 18 for SQL Server is required for SQL server can only be used in synchronous mode.
     connection_string = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;'
     return pyodbc.connect(connection_string)
